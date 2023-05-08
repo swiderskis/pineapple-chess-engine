c89855c92d1fa3bec4a22bbc424cfbc89ff17832
swiderskis <66870324+swiderskis@users.noreply.github.com>
2023-05-08 22:28:55 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    uci::command();
+}
+
+mod uci {
+    use std::io;
+
+    pub fn command() {
+        loop {
+            let mut input = String::new();
+
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {}
+                Err(err) => {
+                    println!("Error parsing command: {err}");
+                    continue;
+                }
+            };
+
+            let mut input: Vec<&str> = input.split_whitespace().collect();
+            let command = input[0];
+            let arguments = input.split_off(1);
+
+            match command.trim() {
+                "uci" => uci(),
+                "" => continue,
+                _ => println!("Unknown command"),
+            }
+        }
+    }
+
+    fn uci() {
+        println!("id name Pineapple");
+        println!("id author Sebastian S.");
+        println!("uciok");
+    }
+}
