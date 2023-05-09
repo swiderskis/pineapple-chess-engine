cbfd94c29e30b878aaff8be1d09a03db5a58471c
swiderskis <66870324+swiderskis@users.noreply.github.com>
2023-05-09 10:33:40 +0100

    Move uci mod to own file

---
+++ src/uci.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+struct Input<'a> {
+    command: &'a str,
+    _arguments: Vec<&'a str>,
+}
+
+pub fn command() {
+    loop {
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(err) => {
+                println!("Error parsing command: {err}");
+                continue;
+            }
+        };
+
+        let input: Vec<&str> = input.split_whitespace().collect();
+        let input = Input {
+            command: input[0],
+            _arguments: input[1..].to_vec(),
+        };
+
+        match input.command.trim() {
+            "uci" => uci(),
+            "isready" => println!("readyok"),
+            "ucinewgame" => continue,
+            "quit" => break,
+            "" => continue,
+            _ => println!("Unknown command"),
+        }
+    }
+}
+
+fn uci() {
+    println!("id name Pineapple");
+    println!("id author Sebastian S.");
+    println!("uciok");
+}
