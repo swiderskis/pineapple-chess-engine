9bae1e28f4e7a6797c672f60a5ee8f392ddd33c7
swiderskis <66870324+swiderskis@users.noreply.github.com>
2023-06-12 16:01:14 +0100

    Remove unneeded comments

--- src/engine.rs
+++ src/engine.rs
@@ -12,15 +12,6 @@ use strum::IntoEnumIterator;
 use strum_macros::{Display, EnumIter, EnumString};
 
 pub fn position() {
-    // let empty_board = String::from("8/8/8/8/8/8/8/8 w - - ");
-    // let start_position = String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ");
-    // let tricky_position =
-    //     String::from("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
-    // let killer_position =
-    //     String::from("rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1 ");
-    // let cmk_position =
-    //     String::from("r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ");
-
     let custom_position =
         String::from("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w Kkq e4 0 1 ");
 
--- src/engine/game.rs
+++ src/engine/game.rs
@@ -340,12 +340,6 @@ impl EnumToInt for CastlingTypes {}
 mod tests {
     use super::*;
 
-    // let tricky_position =
-    //     String::from("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
-    // let killer_position =
-    //     String::from("rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1 ");
-    // let cmk_position =
-    //     String::from("r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ");
     #[test]
     fn tricky_position() {
         let game =
